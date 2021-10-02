74fd6a30bc311a7841495e2057cc476d3e0e8211
georgestein <george.f.stein@gmail.com>
2021-10-02 13:23:20 -0700

    add lens catalogues and data exploration tool

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # ssl-legacysurvey
-Data and codes for self-supervised learning on the DESI legacy surveys
+This directory contains public data and codes for self-supervised learning on the DESI legacy surveys.
+
+## For the live interactive similarity search app please see ***[share.streamlit.io/georgest\
+ein/galaxy_search](https://share.streamlit.io/georgestein/galaxy_search)***, with code available at [github.com/georgestein/galaxy_search](https://github.com/georgestein/galaxy_search)
+
+
+Data can be found here [portal.nersc.gov/project/cusp/ssl_galaxy_surveys/strong_lens_data/
+](https://portal.nersc.gov/project/cusp/ssl_galaxy_surveys/strong_lens_data/)
+
+
+### Create conda environment for data-examples.ipynb
+'''
+conda create --name ssl-legacysurvey python=3.8 matplotlib numpy ipykernel h5py pandas
+
+conda activate ssl-legacysurvey
+
+python -m ipykernel install --user --name ssl-legacysurvey --display-name ssl-legacysurvey
+'''
+
+
---
+++ src/to_rgb.py
@@ -0,0 +1,58 @@
+import numpy as np
+
+"""
+Transformation from raw image data (nanomaggies) to the rgb values displayed
+at the legacy viewer https://www.legacysurvey.org/viewer
+
+Code copied from
+https://github.com/legacysurvey/imagine/blob/master/map/views.py
+"""
+
+def sdss_rgb(imgs, bands, scales=None,
+             m = 0.02):
+    import numpy as np
+    rgbscales = {'u': (2,1.5), #1.0,
+                 'g': (2,2.5),
+                 'r': (1,1.5),
+                 'i': (0,1.0),
+                 'z': (0,0.4), #0.3
+                 }
+    if scales is not None:
+        rgbscales.update(scales)
+
+    I = 0
+    for img,band in zip(imgs, bands):
+        plane,scale = rgbscales[band]
+        img = np.maximum(0, img * scale + m)
+        I = I + img
+    I /= len(bands)
+        
+    # b,g,r = [rimg * rgbscales[b] for rimg,b in zip(imgs, bands)]
+    # r = np.maximum(0, r + m)
+    # g = np.maximum(0, g + m)
+    # b = np.maximum(0, b + m)
+    # I = (r+g+b)/3.
+    Q = 20
+    fI = np.arcsinh(Q * I) / np.sqrt(Q)
+    I += (I == 0.) * 1e-6
+    H,W = I.shape
+    rgb = np.zeros((H,W,3), np.float32)
+    for img,band in zip(imgs, bands):
+        plane,scale = rgbscales[band]
+        rgb[:,:,plane] = (img * scale + m) * fI / I
+
+    # R = fI * r / I
+    # G = fI * g / I
+    # B = fI * b / I
+    # # maxrgb = reduce(np.maximum, [R,G,B])
+    # # J = (maxrgb > 1.)
+    # # R[J] = R[J]/maxrgb[J]
+    # # G[J] = G[J]/maxrgb[J]
+    # # B[J] = B[J]/maxrgb[J]
+    # rgb = np.dstack((R,G,B))
+    rgb = np.clip(rgb, 0, 1)
+    return rgb
+
+def dr2_rgb(rimgs, bands, **ignored):
+    return sdss_rgb(rimgs, bands, scales=dict(g=(2,6.0), r=(1,3.4), z=(0,2.2)), m=0.03)
+
